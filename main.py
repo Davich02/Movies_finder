2fde4ca7c3ff0ea79fd863e003e158c1b26c0094
Davich02 <david.rabotarus@gmail.com>
2026-03-24 20:25:00 +0100

    remove .idea from tracking and cleared main.py

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-import pymysql
-
-config = {
-         'host': 'ich-db.edu.itcareerhub.de',
-         'user': 'ich1',
-         'password': 'password',
-         'database': 'sakila',
-     }
-
-
-with pymysql.connect(**config) as conn:
-    with conn.cursor() as cursor:
-        '''cursor.execute('SHOW TABLES')
-        tables = cursor.fetchall()
-        for table in tables:
-            print(table)'''
-        cursor.execute('DESCRIBE film')
-        film = cursor.fetchall()
-        for film in film:
-            print(film)
-        cursor.execute('DESCRIBE category')
-        category = cursor.fetchall()
-        for category in category:
-            print(category)
-        cursor.execute('DESCRIBE film_category')
-        film_category = cursor.fetchall()
-        for film_category in film_category:
-            print(film_category)
 
 
 
